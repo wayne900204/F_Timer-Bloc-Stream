326c5f7bff64f8ed85ee8875909e6e486c1f737d
redevRx <kasem.jon12@hotmail.com>
2021-01-29 10:09:35 +0700

    bloc + rxDart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:timer_steambuilder/screen/counter_screen.dart';
 
 import 'BlocBase.dart';
 
@@ -16,37 +17,39 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider<IncrementBloc>(
-        bloc: IncrementBloc(),
-        child: CounterPage(),
-      ),
+      home: CounterScreenBloc(),
+      // home: BlocProvider<IncrementBloc>(
+      //   bloc: IncrementBloc(),
+      //   child: CounterPage(),
+      // ),
     );
   }
 }
 
-class CounterPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final IncrementBloc bloc = BlocProvider.of<IncrementBloc>(context);
+// class CounterPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final IncrementBloc bloc = BlocProvider.of<IncrementBloc>(context);
 
-    return Scaffold(
-      appBar: AppBar(title: Text('Wenya Timer'),centerTitle: true,),
-      body: Center(
-        child: StreamBuilder<int>(
-            stream: bloc.outCounter,
-            initialData: 0,
-            builder: (BuildContext context, AsyncSnapshot<int> snapshot){
-              return Text('Look at this number ${snapshot.data}');
-            }
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: (){
-          bloc.incrementCounter.add(null);
-        },
-      ),
-    );
-  }
-}
-
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Wenya Timer'),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: StreamBuilder<int>(
+//             stream: bloc.outCounter,
+//             initialData: 0,
+//             builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+//               return Text('Look at this number ${snapshot.data}');
+//             }),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           bloc.incrementCounter.add(null);
+//         },
+//       ),
+//     );
+//   }
+// }
